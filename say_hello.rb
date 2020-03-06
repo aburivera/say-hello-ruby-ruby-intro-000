416fc195f64bf6600cd7650eaaaa4b8e0833c6fa
Abu Rivera <abuarivera@gmail.com>
2020-03-06 05:20:47 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 def say_hello(name="Ruby Programmer")
   puts "Hello #{name}!"
+=======
+def say_hello(name)
+  puts "say_hello ! #{hello kent beck!}"
+>>>>>>> 89ef55b6ad6249aeaa8e43b798219e0be4b6c4e6
 end
